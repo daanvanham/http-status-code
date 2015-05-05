9a5643b3a6b54dce5702ee6f3b1f42632f61ebd1
Daan van Ham <daan@vanham.io>
2015-05-05 19:49:46 +0200

    created test file updated package.json

---
+++ test/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Lab = require('lab'),
+	lab = exports.lab = Lab.script(),
+	Code = require('code'),
+	HTTPStatusCode = require('../index'),
+	protocols = ['HTTP/1.0', 'HTTP/1.1', 'WEBDAV'],
+	definitions = HTTPStatusCode.getProtocolDefinitions();
+
+protocols.forEach(function(protocol) {
+	lab.experiment(protocol, function() {
+		Object.keys(definitions).forEach(function(status) {
+			lab.test(status, function(done) {
+				var message = HTTPStatusCode.getMessage(status, protocol),
+					expectation = HTTPStatusCode.getProtocolDefinitions(protocol)[status] || 'Unknown';
+
+				Code.expect(message).to.equal(expectation);
+				done();
+			});
+		});
+	});
+});
