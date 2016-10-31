552a768d3268de716e34fdb260e6bc0ca1299425
Rogier Spieker <rogier@konfirm.net>
2016-10-31 21:18:04 +0100

    Added "103 - Early Hints" status
    json/misc.json
    + Added “103 - Early Hints” status, as proposed in
    https://tools.ietf.org/html/draft-kazuho-early-hints-status-code-00
    
    test/index.js
    * added tests for “misc”, actually testing every possible status code
    (this includes all types in “misc”)

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var Lab = require('lab'),
 	lab = exports.lab = Lab.script(),
 	Code = require('code'),
 	HTTPStatusCode = require('../lib'),
-	protocols = ['HTTP/1.0', 'HTTP/1.1', 'HTTP/2', 'WEBDAV'],
+	protocols = ['HTTP/1.0', 'HTTP/1.1', 'HTTP/2', 'WEBDAV', 'misc'],
 	definitions = HTTPStatusCode.getProtocolDefinitions();
 
 protocols.forEach(function(protocol) {
